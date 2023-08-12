6c66871d4c7f663416edf7de8787051bb28da816
tomohiro.yamaoka <overusevery@gmail.com>
2023-08-12 15:31:10 +0900

    [2]add page links as common layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <navi>
+        <Link href="/getstarted">Get Started</Link>
+        <Link href="/setting">Setting</Link>
+      </navi>
       <body className={inter.className}>{children}</body>
     </html>
   )
