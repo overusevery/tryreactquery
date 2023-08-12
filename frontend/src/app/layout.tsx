fd29bacefe3d0abb305e9cdff6080ded13571df4
overusevery <69046254+overusevery@users.noreply.github.com>
2023-08-13 01:23:48 +0900

    Feature/2 (#4)
    * [2]add get started page
    
    * [2]add setting page
    
    * [2]add page links as common layout
    
    * [2]arrage layout of sidebar
    
    * [2]add cypress
    
    * [2]add cypress navigation test
    
    * [2]add cypress navigation test
    
    * [2]edit Home visual style

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +16,25 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html className={inter.className}lang="en">
+      <body className='container' >
+        <navi className='sidebar'>
+          <ul>
+            <li key='0'>
+              <Link href="/">Home</Link>
+            </li>
+            <li key='1'>
+              <Link href="/getstarted">Get Started</Link>
+            </li>
+            <li key='2'>
+              <Link href="/setting">Setting</Link>
+            </li>
+          </ul>
+        </navi>
+        <div className='main-content'>
+          {children}
+        </div>
+      </body>
     </html>
   )
 }
