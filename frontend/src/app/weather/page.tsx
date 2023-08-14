cbc7a68e099703ff392ab1613f83495d7e34e2e8
tomohiro.yamaoka <overusevery@gmail.com>
2023-08-14 15:28:09 +0900

    [5]extract get request function

--- frontend/src/app/weather/page.tsx
+++ frontend/src/app/weather/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 import { useQuery } from 'react-query'
 
+function getForcast(){
+    return fetch('https://www.jma.go.jp/bosai/forecast/data/overview_forecast/130000.json')
+      .then(res => res.json())
+}
+
 export default function Weather() {
-  const { isLoading, error, data } = useQuery('repoData', () =>
-    fetch('https://www.jma.go.jp/bosai/forecast/data/overview_forecast/130000.json').then(res =>
-      res.json()
-    )
-  )
+  const { isLoading, error, data } = useQuery('forcast', getForcast)
     return (
       <div>
         <h1>Weather</h1>
