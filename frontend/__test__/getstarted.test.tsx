5039ca59bcf1819e2a1cf2add5070c638a711498
tomohiro.yamaoka <overusevery@gmail.com>
2023-08-19 23:57:19 +0900

    [5]add vitest + testing library init test

---
+++ frontend/__test__/getstarted.test.tsx
@@ -0,0 +1,9 @@
+
+import {render, screen} from '@testing-library/react'
+import GetStarted from '../src/app/getstarted/page';
+
+test('getstarted has header message', async () => {
+  render(<GetStarted />)
+
+  expect(screen.getByText('Get Started')).toBeInTheDocument()
+})
---
+++ frontend/vitest.setup.ts
@@ -0,0 +1,4 @@
+import matchers from '@testing-library/jest-dom/matchers'
+import { expect } from 'vitest';
+
+expect.extend(matchers);//see https://zenn.dev/ogakuzuko/scraps/584b9fe0d23155
