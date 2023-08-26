d76ed90075aa40d22ec3af5fbe1768b441b4470d
tomohiro.yamaoka <overusevery@gmail.com>
2023-08-26 17:42:44 +0900

    [5]run format

--- frontend/__test__/utils.tsx
+++ frontend/__test__/utils.tsx
@@ -2,8 +2,8 @@
 import { render } from "@testing-library/react";
 import * as React from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
-import { makeServer } from "../src/mocks/server"
-makeServer()
+import { makeServer } from "../src/mocks/server";
+makeServer();
 
 const createTestQueryClient = () =>
   new QueryClient({
--- frontend/__test__/weather.test.tsx
+++ frontend/__test__/weather.test.tsx
@@ -5,5 +5,11 @@ import { renderWithClient } from "./utils";
 test("weather has header message", async () => {
   renderWithClient(await WeatherHome());
   expect(screen.getByText("Weather")).toBeInTheDocument();
-  await waitFor(() => expect(screen.getByText("[mock]関東甲信地方は、高気圧に覆われています。", {exact: false})).toBeInTheDocument());
+  await waitFor(() =>
+    expect(
+      screen.getByText("[mock]関東甲信地方は、高気圧に覆われています。", {
+        exact: false,
+      }),
+    ).toBeInTheDocument(),
+  );
 });
