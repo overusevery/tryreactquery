823f52900c5fcdadeffda74d145663e88303b079
tomohiro.yamaoka <overusevery@gmail.com>
2023-08-27 12:14:40 +0900

    [5]add error handling for weather forcast

--- frontend/src/app/weather/weatherforcast.tsx
+++ frontend/src/app/weather/weatherforcast.tsx
@@ -4,15 +4,19 @@ import Skeleton from '@mui/material/Skeleton';
 
 async function getForcast(){
     const res = await fetch('https://www.jma.go.jp/bosai/forecast/data/overview_forecast/130000.json');
+    if(!res.ok){
+      throw new Error(`[${res.status}] error`)
+    }
   return await res.json();
 }
 
 export default function Forcast() {
-  const { isLoading, error, data } = useQuery('forcast', getForcast)
+  const { isLoading, data, isError } = useQuery('forcast', getForcast)
     return (
       <div>
         <h2>Forcast(Tokyo)</h2>
         <div>{isLoading ? <Skeleton sx={{ bgcolor: 'grey.900' }} animation="wave" />: data?.text}</div>
+        <div>{isError && "Sorry Failed to get Forcast..."}</div>
       </div>
     )
   }
--- frontend/src/mocks/server.js
+++ frontend/src/mocks/server.js
@@ -1,4 +1,5 @@
-import { createServer } from "miragejs"
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { createServer, Response } from "miragejs" //Response will be used for error mode
 
 export function makeServer({ environment = "test" } = {}) {
   let server = createServer({
@@ -13,6 +14,10 @@ export function makeServer({ environment = "test" } = {}) {
             "text": "[mock]関東甲信地方は、高気圧に覆われています。\n\n　東京地方は、曇りや晴れで、雨や雷雨となっている所があります。\n\n　２６日は、高気圧に覆われますが、湿った空気や上空の寒気の影響を受ける見込みです。このため、曇り時々晴れで、雨や雷雨となり、夜のはじめ頃にかけて激しく降る所があるでしょう。伊豆諸島でも、雷を伴って激しい雨の降る所がある見込みです。\n\n　２７日は、引き続き高気圧に覆われますが、湿った空気や上空の寒気の影響を受ける見込みです。このため、曇りで朝は晴れますが、雨や雷雨となり、昼前から昼過ぎにかけて激しく降る所があるでしょう。伊豆諸島でも、雷を伴って激しい雨の降る所がある見込みです。\n\n【関東甲信地方】\n　関東甲信地方は、曇りや晴れで雷を伴った激しい雨の降っている所があります。\n\n　２６日は、高気圧に覆われますが、湿った空気や上空の寒気の影響を受ける見込みです。このため、曇りや晴れで、雨や雷雨となり非常に激しく降る所があるでしょう。\n\n　２７日は、引き続き高気圧に覆われますが、湿った空気や上空の寒気の影響を受ける見込みです。このため、曇りや晴れで、雨や雷雨となり激しく降る所があるでしょう。\n\n　関東地方と伊豆諸島の海上では、２６日は波がやや高く、２７日はうねりを伴い波がやや高い見込みです。"
         }
       })
+      //ToDo:create error mode
+      //this.get("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/130000.json", () => {
+      //  return new Response(400, { some: 'header' }, { errors: [ 'name cannot be blank'] });
+      //})
     },
   })
 
